58831d49a8521c7e87c8e8da68f1fa66f4917cfd
joseph dias <josephdias92@gmail.com>
2016-11-20 14:12:00 +0530

    initial build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ResumePageComponent } from './resume-page/resume-page.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/resume',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path:'resume',
+    component:ResumePageComponent
+  },
+  {
+    path:'contact',
+    component:ContactMeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ResumePageComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
